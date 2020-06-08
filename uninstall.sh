087c036294c02e644952f14cf70db53a5e6c2c6e
Carter C <cclements3150@gmail.com>
2020-06-07 21:55:50 -0400

    [INIT] Removes the script from the bin

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Running me will remove the frank script from /usr/local/bin/ directory.
+# However i will not delete these files, you can do that urself.
+
+rm /usr/local/bin/frank
+
+echo "If frank existed, he's gone now."
